fd5bfb5526703a2c8b14d097787b766ef7dd959c
Umidbek Karimov <uma.karimov@gmail.com>
2018-08-23 15:20:43 +0500

    test(ClassParseResult): Add tests for arrow function properties.

--- src/parser/parsers/__tests__/class-parse-result/__fixtures__/class-properties.ts
+++ src/parser/parsers/__tests__/class-parse-result/__fixtures__/class-properties.ts
@@ -1,4 +1,4 @@
-export class ClassMembers {
+export class ClassProperties {
   // @ts-ignore
   public a: number;
 
@@ -7,4 +7,8 @@ export class ClassMembers {
 
   // @ts-ignore
   private c: Promise<boolean>;
+
+  public d = () => this.b;
+
+  public e = (a: number) => a + this.a;
 }
